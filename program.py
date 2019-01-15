cffb98a62c3cd6e018273af6f519bda27b84c92e
Roller Angel <techangel428@gmail.com>
2019-01-14 23:50:05 -0700

    Added working program

---
+++ program.py
@@ -0,0 +1,14 @@
+def print_header():
+    print("--------------------------------------")
+    print("      ROCK, PAPER, SCISSORS APP")
+    print("--------------------------------------")
+    print()
+
+
+def main():
+    print_header()
+    name = input("What is your name my friend? ")
+
+
+if __name__ == "__main__":
+    main()
